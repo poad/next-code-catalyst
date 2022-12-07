f11c6d64eebd92929543674b76abcf894a7e98d9
Kenji Saito <1867845+poad@users.noreply.github.com>
2022-12-07 19:05:05 +0900

    Initialize for CodeCatalyst (#1)

---
+++ cdk/lib/frontend-stack.ts
@@ -0,0 +1,47 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3_deployment from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+
+export class FrontendStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const frontendSourceBucket = new s3.Bucket(this, "FrontendAppBucket", {
+      websiteIndexDocument: "index.html",
+    })
+
+    const frontendOriginAccessIdentity = new cloudfront.OriginAccessIdentity(this, "FrontendAppOIA", {
+      comment: "Access from CloudFront to the bucket."
+    })
+
+    frontendSourceBucket.grantRead(frontendOriginAccessIdentity)
+
+    const frontendCloudfront = new cloudfront.CloudFrontWebDistribution(this, "FrontendAppCloudFront", {
+      originConfigs: [{
+        s3OriginSource: {
+          s3BucketSource: frontendSourceBucket,
+          originAccessIdentity: frontendOriginAccessIdentity
+        },
+        behaviors: [{isDefaultBehavior: true}]
+      }],
+      errorConfigurations: [{
+        errorCode: 404,
+        errorCachingMinTtl: 0,
+        responseCode: 200,
+        responsePagePath: "/index.html"
+      }]
+    })
+
+    new s3_deployment.BucketDeployment(this, "FrontendAppDeploy", {
+      sources: [s3_deployment.Source.asset("frontend/build")],
+      destinationBucket: frontendSourceBucket,
+      distribution: frontendCloudfront,
+      distributionPaths: ["/*"],
+      memoryLimit: 1024
+    })
+
+    new cdk.CfnOutput(this, "AppURL", {value: `https://${frontendCloudfront.distributionDomainName}/`})
+  }
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import { AppProps } from 'next/app';
+import * as React from 'react';
+
+const App = ({ Component }: AppProps): JSX.Element => <Component />;
+
+export default App;
