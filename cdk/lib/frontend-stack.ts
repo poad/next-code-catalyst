e408c37740eb2a4f14a4d31cbc31065d7589f5dd
Kenji Saito <ken-yo@mbr.nifty.com>
2024-09-08 13:38:28 +0900

    Bumps node modules

--- cdk/lib/frontend-stack.ts
+++ cdk/lib/frontend-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as s3_deployment from 'aws-cdk-lib/aws-s3-deployment';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 
@@ -12,35 +13,31 @@ export class FrontendStack extends cdk.Stack {
       websiteIndexDocument: 'index.html',
     });
 
-    const frontendOriginAccessIdentity = new cloudfront.OriginAccessIdentity(
+    const frontendOriginAccessControl = new cloudfront.S3OriginAccessControl(
       this,
-      'FrontendAppOIA',
+      'OAC',
       {
-        comment: 'Access from CloudFront to the bucket.',
+        description: 'Access from CloudFront to the bucket.',
+        originAccessControlName: 's3',
       },
     );
 
-    frontendSourceBucket.grantRead(frontendOriginAccessIdentity);
-
-    const frontendCloudfront = new cloudfront.CloudFrontWebDistribution(
+    const frontendCloudfront = new cloudfront.Distribution(
       this,
       'FrontendAppCloudFront',
       {
         comment: 'example by CodeCatalyst',
-        originConfigs: [
+        defaultBehavior: {
+          origin: origins.S3BucketOrigin.withOriginAccessControl(frontendSourceBucket, {
+            originAccessControl: frontendOriginAccessControl,
+          }),
+
+        },
+        errorResponses: [
           {
-            s3OriginSource: {
-              s3BucketSource: frontendSourceBucket,
-              originAccessIdentity: frontendOriginAccessIdentity,
-            },
-            behaviors: [{ isDefaultBehavior: true }],
-          },
-        ],
-        errorConfigurations: [
-          {
-            errorCode: 404,
-            errorCachingMinTtl: 0,
-            responseCode: 200,
+            httpStatus: 404,
+            ttl: cdk.Duration.millis(0),
+            responseHttpStatus: 200,
             responsePagePath: '/index.html',
           },
         ],
